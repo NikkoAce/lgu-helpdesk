36e1f0e4c3a1ffc8015b21dab66371b05a44c08a
NikkoAce <moreno.nikkaoce@gmail.com>
2025-08-26 17:44:55 +0800

    permission

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,6 @@ const authMiddleware = require('../middleware/authMiddleware');
 // This new route will allow other systems to verify a user's token.
 // It's placed before other routes with parameters to avoid conflicts.
 router.get('/me', authMiddleware, getMe);
-
 router.get('/', authMiddleware, getAllUsers);
 router.patch('/:id', authMiddleware, updateUser);
 router.delete('/:id', authMiddleware, deleteUser);
