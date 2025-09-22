0ac20bfd1c0400adff9d5035c51c32ec6b1dffa4
NikkoAce <moreno.nikkaoce@gmail.com>
2025-09-22 11:58:55 +0800

    fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -85,6 +85,7 @@ exports.forgotPassword = async (req, res) => {
             const transporter = nodemailer.createTransport({
                 host: process.env.EMAIL_HOST,
                 port: process.env.EMAIL_PORT,
+                secure: process.env.EMAIL_PORT == 465, // true for 465, false for other ports
                 auth: {
                     user: process.env.EMAIL_USERNAME,
                     pass: process.env.EMAIL_PASSWORD,
