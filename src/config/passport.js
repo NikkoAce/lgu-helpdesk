9b3241d207a3aadd370be301f1d42f4160fbdd79
NikkoAce <moreno.nikkaoce@gmail.com>
2025-09-26 21:06:23 +0800

    fix filepath

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,6 +1,6 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const passport = require('passport');
-const User = require('../../features/users/user.model.js');
+const User = require('../features/users/user.model.js');
 
 passport.use(
     new GoogleStrategy(
